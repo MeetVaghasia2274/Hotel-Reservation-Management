8839e7305b75669f77c7956c2fd1bfe764f63772
MeetVaghasia2274 <meetvaghasia78@gmail.com>
2025-05-19 17:59:06 +0530

    Initial Commit

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/hotel_system";
-    private static final String USER = "root"; // replace with your MySQL username
-    private static final String PASSWORD = "Skpi051wu0s"; // replace with your MySQL password
+    private static final String USER = "_"; // replace with your MySQL username
+    private static final String PASSWORD = "_"; // replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
